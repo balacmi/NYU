23ebbf5c28534ecb96d15d6076c335a67b155958
balacm <milos.balac@ivt.baug.ethz.ch>
2018-07-06 10:48:05 -0400

    moving binding of the routing module to the BikeshareModule

--- bikesharing/src/main/java/nyu/matsim/bikesharing/RunBikesharing.java
+++ bikesharing/src/main/java/nyu/matsim/bikesharing/RunBikesharing.java
@@ -2,13 +2,11 @@ package nyu.matsim.bikesharing;
 
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 
 import nyu.matsim.bikesharing.config.BikesharingConfigGroup;
 import nyu.matsim.bikesharing.qsim.BikeshareQsimModule;
-import nyu.matsim.bikesharing.router.BikeshareRoutingModule;
 
 /**
  * Hello world!
@@ -21,17 +19,7 @@ public class RunBikesharing {
 
 		Controler controler = new Controler(config);
 		controler.getConfig().controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
-		controler.addOverridingModule(new AbstractModule() {
-			
-			@Override
-			public void install() {
-				addRoutingModuleBinding("bikeshare").to(BikeshareRoutingModule.class);
-				
-			}
-			
-			
-		});
-		
+
 		controler.addOverridingModule(new BikeshareQsimModule());
 		controler.addOverridingModule(new BikeshareModule());
 		controler.run();
