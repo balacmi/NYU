9a3530e914d6632b0ddfdb5c5ced3758eec841dc
balacm <milos.balac@ivt.baug.ethz.ch>
2018-07-06 10:49:19 -0400

    adding a fleet size parameter

--- bikesharing/src/main/java/nyu/matsim/bikesharing/config/BikesharingConfigGroup.java
+++ bikesharing/src/main/java/nyu/matsim/bikesharing/config/BikesharingConfigGroup.java
@@ -2,11 +2,23 @@ package nyu.matsim.bikesharing.config;
 
 import org.matsim.core.config.ReflectiveConfigGroup;
 
-public class BikesharingConfigGroup extends ReflectiveConfigGroup{
+public class BikesharingConfigGroup extends ReflectiveConfigGroup {
 
 	private static final String GROUP_NAME = "bikesharing";
+	private int fleetSize = 100;
+
 	public BikesharingConfigGroup() {
 		super(GROUP_NAME);
+	}
+
+	@StringSetter("fleetSize")
+	public void setFleetSize(final String fleetSize) {
+		this.fleetSize = Integer.parseInt(fleetSize);
+	}
+
+	@StringGetter("fleetSize")
+	public int getFleetSize() {
+		return this.fleetSize;
 	}
 
 }
